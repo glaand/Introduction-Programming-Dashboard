f21fc6ad4be648560bf47e04c60f10edb88dc038
Alex Schneider <sjessandru.schneider@gmail.com>
2021-11-18 16:17:24 +0100

    Namensänderung File Daten zu data.csv load_data Funktion programmiert

--- aufgabenblatt_8.py
+++ aufgabenblatt_8.py
@@ -1,1 +1,23 @@
-# Alex & André
+'''
+Author: Alex
+Date: 18. 11. 2021
+Programming lanugage: Python 3.8.8
+'''
+
+# imports
+import pandas as pd
+import os
+
+# Constants
+DATA_PATH = os.path.join("datasets", "data.csv")
+
+
+def load_data(path=None):
+    if path is None:
+        path = DATA_PATH
+    with open(path) as file:
+        return pd.read_csv(file)
+
+
+if __name__ == "__main__":
+    df = load_data()
