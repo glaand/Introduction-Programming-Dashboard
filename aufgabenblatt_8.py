ee286781fa6627191a575a0770488119f3b069cf
André Glatzl <andreglatzl@gmail.com>
2021-11-22 11:28:41 +0000

    Finished exercise 39

--- aufgabenblatt_8.py
+++ aufgabenblatt_8.py
@@ -63,6 +63,10 @@ def filterDF(**kwargs) -> pd.DataFrame:
         return pd.DataFrame([])
     return kwargs['data'].loc[kwargs['data'][kwargs['column']] == kwargs['key']]
 
+def showDash(elements: list):
+    app = dash.Dash(__name__)
+    app.layout = html.Div(elements)
+    app.run_server(debug=True, host="0.0.0.0", port=9999)
 
 if __name__ == "__main__":
     def exercise36():
@@ -86,12 +90,13 @@ if __name__ == "__main__":
         filteredByDiscipline = filterDF(data=df, column="Discipline", key="Judo")
         print(f"a) Daten gefiltert nach dem Land Schweiz: \n{filteredByCountry} \nb) Daten gefiltert nach Disciplin Judo: \n{filteredByDiscipline}")
 
-        app = dash.Dash(__name__)
-        app.layout = html.Div([
-            html.Title("Verbindung Disziplin mit Land:"),
+        showDash([
+            html.P("Aufgabe 38"),
+            html.P("Verbindung Disziplin mit Land:"),
             dcc.Graph(
                 id="graph", 
-                figure=px.scatter(df, 
+                figure=px.scatter(
+                    df, 
                     x="Country",
                     y="Discipline",
                     color="Medal",
@@ -102,11 +107,47 @@ if __name__ == "__main__":
             ),
         ])
 
-        app.run_server(debug=True, host="0.0.0.0", port=9999)
-
-    exercise38()
+    # exercise38()
 
     def exercise39():
-        pass
+        df = load_data()
+        # Filter to only show One Country
+        df2 = filterDF(data=df, column="Country", key="Switzerland")
+        # Filter to only display Gold 
+        df3 = filterDF(data=df2, column="Medal", key="Gold")
+        # Group all the Gold Medals per Year
+        df4 = df3.groupby(["Year"]).size().reset_index(name="Gold")
+        showDash([
+            html.P("Aufgabe 39"),
+            html.P("Streudiagramm: Land pro Disziplin"),
+            dcc.Graph(
+                id="scatter", 
+                figure=px.scatter(
+                    df, 
+                    x="Country",
+                    y="Discipline",
+                    color="Medal",
+                    # Farben für Punkte: Gold = FFD700 | Silber = C0C0C0 | Bronze = CD7F32
+                    color_discrete_sequence=["#FFD700", "#C0C0C0", "#CD7F32"]
+                )
+            ),
+            html.P("Histogramm von Anzahl Medaillen pro Land"),
+            dcc.Graph(
+                id="histo", 
+                figure=px.histogram(
+                    df, 
+                    x="Country",
+                )
+            ),
+            html.P("Liniendiagramm: Anzahl Gold-Medaillen von der Schweiz über die Jahre"),
+            dcc.Graph(
+                id="line", 
+                figure=px.line(
+                    df4, 
+                    x="Year",
+                    y="Gold",
+                )
+            ),
+        ])
 
-    # exercise39()
+    exercise39()
