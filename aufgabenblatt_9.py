01214afeff607fd84e257fef1afeed4bb2694d41
Alex Schneider <sjessandru.schneider@gmail.com>
2021-12-01 10:49:04 +0100

    Aufgabenblatt 9 erstellt mit Funktionen

---
+++ aufgabenblatt_9.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# =============================================================================
+# Created By  : Alexandru Schneider
+# Credits     : André Glatzl
+# Created Date: 01. 12. 2021
+# =============================================================================
+"""Aufgabenblatt 9"""
+
+
+def aufgabe41():
+    pass
+
+
+def aufgabe42():
+    pass
+
+
+def aufgabe43():
+    pass
+
+
+def aufgabe44():
+    pass
+
+
+def aufgabe45():
+    pass
+
+
+def main():
+    aufgabe41()
+    aufgabe42()
+    aufgabe43()
+    aufgabe44()
+    aufgabe45()
+
+
+if __name__ == "__main__":
+    main()
