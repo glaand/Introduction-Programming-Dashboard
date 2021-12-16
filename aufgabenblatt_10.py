90e526270a71f4182661d96a3ca7c8bad2d13b8d
André Glatzl <andreglatzl@gmail.com>
2021-12-16 15:30:37 +0000

    Finished exercise aufgabe 38

--- aufgabenblatt_10.py
+++ aufgabenblatt_10.py
@@ -11,6 +11,7 @@
 # =============================================================================
 # Imports
 # =============================================================================
+from traitlets.traitlets import default
 from aufgabenblatt_9 import load_data
 import pandas as pd
 
@@ -24,22 +25,24 @@ def filter_data(df: pd.DataFrame, land: str, jahr: int, sportart: str) -> pd.Dat
         df = df[df["Discipline"] == sportart]
     return df
 
-def group_medals_by_country(df: pd.DataFrame, country: str) -> pd.DataFrame:
-    df = filter_data(df, country, None, None)
-    return df
-
 def filter_data2(df: pd.DataFrame, string: str) -> pd.DataFrame:
     df = df[df["Athlete"].str.contains(string)]
+    return df
+
+def group_medals_by_country(df: pd.DataFrame, country: str) -> pd.DataFrame:
+    df = filter_data(df, country, None, None)
+    df = df.groupby(["Country", "Medal"]).size().reset_index(name="Count")
+    df = df.drop("Country", axis=1).set_index("Medal").transpose()
     return df
 
 
 if __name__ == "__main__":
     data = load_data()
     # Aufgabe 46
-    result_data_frame = filter_data(data, "Switzerland", 2008, "Swimming")
+    # result_data_frame = filter_data(data, "Switzerland", 2008, "Swimming")
 
     # Aufgabe 47
-    searched_data_frame = filter_data2(data, "Ale")
+    # searched_data_frame = filter_data2(data, "Ale")
 
     # Aufgabe 48
     medals_grouped_by_country = group_medals_by_country(data, "Switzerland")
