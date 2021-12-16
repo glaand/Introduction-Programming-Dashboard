e70eff60a3a09b79434b51a6859343aafedc8bdd
André Glatzl <andreglatzl@gmail.com>
2021-12-16 15:10:44 +0000

    some changes

--- aufgabenblatt_10.py
+++ aufgabenblatt_10.py
@@ -17,15 +17,22 @@ import pandas as pd
 
 def filter_data(df: pd.DataFrame, land: str, jahr: int, sportart: str) -> pd.DataFrame:
     if land is not None:
-        df = df[df["Country" == land]]
+        df = df[df["Country"] == land]
     if jahr is not None:
-        df = df[df["Year" == jahr]]
+        df = df[df["Year"] == jahr]
     if sportart is not None:
-        df = df[df["Discipline" == sportart]]
+        df = df[df["Discipline"] == sportart]
     return df
 
+def group_medals_by_country(df: pd.DataFrame, country: str) -> pd.DataFrame:
+    df = filter_data(df, country, None, None)
+    return df
 
 if __name__ == "__main__":
     data = load_data()
     # Aufgabe 46
-    result_data_frame = filter_data(data, "Schweiz", 2008, "Swimming")
+    # result_data_frame = filter_data(data, "Schweiz", 2008, "Swimming")
+
+    # Aufgabe 48
+    medals_grouped_by_country = group_medals_by_country(data, "Switzerland")
+    print(medals_grouped_by_country)
