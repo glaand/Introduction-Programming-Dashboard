e0837697ffbda9099361f28a0fdc2c52b55a4063
Alex Schneider <sjessandru@gmail.com>
2021-12-17 09:20:31 +0100

    Aufgabe 47 case sensitive

--- aufgabenblatt_10.py
+++ aufgabenblatt_10.py
@@ -13,6 +13,7 @@
 # =============================================================================
 from aufgabenblatt_9 import load_data
 import pandas as pd
+import re
 
 
 def filter_data(df: pd.DataFrame, land: str, jahr: int, sportart: str) -> pd.DataFrame:
@@ -21,26 +22,37 @@ def filter_data(df: pd.DataFrame, land: str, jahr: int, sportart: str) -> pd.Dat
     if jahr is not None:
         df = df[df["Year"] == jahr]
     if sportart is not None:
-        df = df[df["Discipline"] == sportart]
+        df = df[df["Sport"] == sportart]
     return df
+
 
 def group_medals_by_country(df: pd.DataFrame, country: str) -> pd.DataFrame:
     df = filter_data(df, country, None, None)
     return df
 
-def filter_data2(df: pd.DataFrame, string: str) -> pd.DataFrame:
-    df = df[df["Athlete"].str.contains(string)]
-    return df
+
+def filter_data2(df, string: str):
+    pattern = ""
+    for char in string:
+        pattern += f"(?=.*{str(char)})"
+    pattern += ".*"
+    return df[df["Athlete"].str.match(pattern)]
 
 
 if __name__ == "__main__":
+    # load data from csv as dataframe
     data = load_data()
+    # change Year column to integer
+    data["Year"] = data["Year"].astype(int)
+
     # Aufgabe 46
-    result_data_frame = filter_data(data, "Switzerland", 2008, "Swimming")
+    result_data_frame = filter_data(data, "Switzerland", 2008, "Cycling")
+    #print(f"Aufgabe 46: \n{result_data_frame.head()}")
 
     # Aufgabe 47
-    searched_data_frame = filter_data2(data, "Ale")
+    searched_data_frame = filter_data2(data, "zv")
+    print(f"Aufgabe 47: \n{searched_data_frame['Athlete']}")
 
     # Aufgabe 48
     medals_grouped_by_country = group_medals_by_country(data, "Switzerland")
-    print(medals_grouped_by_country)
+    #print(f"Aufgabe 48: \n{medals_grouped_by_country.head()}")
