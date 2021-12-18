63e6b7256d26f134407d4ed108c606144dfcb805
Alex Schneider <sjessandru@gmail.com>
2021-12-18 19:44:48 +0100

    Aufgabe 49 erledigt

--- aufgabenblatt_10.py
+++ aufgabenblatt_10.py
@@ -5,14 +5,16 @@
 # Credits     : Alexandru Schneider
 # Created Date: 16. 12. 2021
 # =============================================================================
-"""Aufgabenblatt 8"""
-
+"""Aufgabenblatt 10"""
 
 # =============================================================================
 # Imports
 # =============================================================================
+from aufgabenblatt_8 import showDash
 from aufgabenblatt_9 import load_data
 import pandas as pd
+from dash import html, dcc
+import plotly.express as px
 
 
 def filter_data(df: pd.DataFrame, land: str, jahr: int, sportart: str) -> pd.DataFrame:
@@ -40,7 +42,98 @@ def group_medals_by_country(df: pd.DataFrame, country: str) -> pd.DataFrame:
     return df
 
 
-if __name__ == "__main__":
+def aufgabe49(data: pd.DataFrame):
+    df_usa = normalizeDataFrame(countMedalPerYearForCountry(data, "United States"))
+    df_sov = normalizeDataFrame(countMedalPerYearForCountry(data, "Soviet Union"))
+    df_wger = normalizeDataFrame(countMedalPerYearForCountry(data, "West Germany"))
+    df_eger = normalizeDataFrame(countMedalPerYearForCountry(data, "East Germany"))
+    df_dis = countDisciplinesPerYear(data).sort_values("Count")
+    return showDash([
+        html.H1("Aufgabe 49"),
+        html.Div(
+            children=[
+                html.H3("Grafik 1 - USA fällt 1980 aus"),
+                dcc.Graph(
+                    id="USA_Bar",
+                    figure=plotCountPerYearsForCountry(df_usa)
+                )
+            ]
+        ),
+        html.Div(
+            children=[
+                html.H3("Grafik 2 - Soviet Union fällt 1984 aus und ab 1988 keine Medaillen mehr"),
+                dcc.Graph(
+                    id="SOV_bar",
+                    figure=plotCountPerYearsForCountry(df_sov)
+                )
+            ]
+        ),
+        html.Div(
+            children=[
+                html.H3("Grafik 3 - West Deutschland fällt 1980 aus und holt ab 1988 keine Medaillen mehr"),
+                dcc.Graph(
+                    id="WEST_GER_bar",
+                    figure=plotCountPerYearsForCountry(df_wger)
+                )
+            ]
+        ),
+        html.Div(
+            children=[
+                html.H3("Grafik 4 - Ost Deutschland fällt 1984 aus und holt ab 1988 keine Medaillen mehr"),
+                dcc.Graph(
+                    id="EAST_GER_bar",
+                    figure=plotCountPerYearsForCountry(df_eger)
+                )
+            ]
+        ),
+        html.Div(
+            children=[
+                html.H3("Grafik 5 - Soviet Union ist zweithöchster Medaillengewinner obwohl sie nur 3 mal teilgenommen haben!!"),
+                dcc.Graph(
+                    id="Countries_All_Years",
+                    figure=px.bar(df_dis.tail(20),
+                                  x="Country",
+                                  y="Count")
+                )
+            ]
+        )
+    ])
+
+
+def countMedalPerYearForCountry(df: pd.DataFrame, country: str) -> pd.DataFrame:
+    return filter_data(df, country, None, None).groupby("Year").size().reset_index(name="Count")
+
+
+# some Countries didnt win medals every Olympic games
+def normalizeDataFrame(df: pd.DataFrame) -> pd.DataFrame:
+    normalizedDataFrame = pd.DataFrame(columns=["Year", "Count"])
+    for year in range(1976, 2012, 4):
+        dff = df[df["Year"] == year]
+        # Falls in einem Jahr keine Medaillen geholt wurde
+        if len(dff.index) != 0:
+            count = dff.iloc[0, 1]
+        else:
+            count = 0
+        rows = {"Year": year, "Count": count}
+        normalizedDataFrame = normalizedDataFrame.append(rows, ignore_index=True)
+    return normalizedDataFrame
+
+
+def plotCountPerYearsForCountry(df: pd.DataFrame):
+    figure = px.bar(
+        df,
+        x="Year",
+        y="Count"
+    )
+    figure.update_xaxes(type="category")
+    return figure
+
+
+def countDisciplinesPerYear(df: pd.DataFrame) -> pd.DataFrame:
+    return df.groupby("Country").size().reset_index(name="Count")
+
+
+def main():
     # load data from csv as dataframe
     data = load_data()
     # change Year column to integer
@@ -57,3 +150,16 @@ if __name__ == "__main__":
     # Aufgabe 48
     medals_grouped_by_country = group_medals_by_country(data, "Switzerland")
     print(f"Aufgabe 48: \n{medals_grouped_by_country.head()}")
+
+    # Aufgabe 49
+    # 1. Grafik: USA Balkendiagramm über die Jahre
+    # 2. Grafik: Soviet Union Balkendiagramm
+    # 3. Grafik: West Germany Balkendiagramm
+    # 4. Grafik: East Germany Balkendiagramm
+    # 5. Grafik: Count Medals for each Country over ALL Years
+    app = aufgabe49(data)
+    app.run_server(debug=True, host="0.0.0.0", port=9999)
+
+
+if __name__ == "__main__":
+    main()
