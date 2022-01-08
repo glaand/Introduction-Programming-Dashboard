bcbf251c76f7e87a853898c5b7ce4381c21bedc2
André Glatzl <andreglatzl@gmail.com>
2022-01-08 13:28:38 +0000

    changes

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ def main():
 # server --> for heroku to start the server (Procfile)
 app, server = main()
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, host="0.0.0.0")
